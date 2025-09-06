9ec0110f9429818feaa42fc0245c9a264e583707
onomylock <onomylock@outlook.com>
2025-09-06 11:23:14 +0700

    fix

--- angular-todo/src/app/components/todo-item/item-done/item-done.component.ts
+++ angular-todo/src/app/components/todo-item/item-done/item-done.component.ts
@@ -1,4 +1,4 @@
-import {Component, isStandalone} from '@angular/core';
+import {Component} from '@angular/core';
 import { ItemBaseDirective } from '../item-base/item-base.directive';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
